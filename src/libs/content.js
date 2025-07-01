5ae4efe3c83f310448f984938f2392a3bfea2bc6
Niklas Siefke <niklas@mazemap.com>
2025-07-01 16:15:05 +0200

    Revert "Add support for 'checkin' content type"
    This reverts commit 4cb9b58fcab8d46c7389302c823117f629e153ef.

--- src/libs/content.js
+++ src/libs/content.js
@@ -65,15 +65,10 @@ const content = {
 		const dir = (process.env.CONTENT_DIR || 'src').replace(/\/$/, '')
 		const [year, month, day] = date.toISOString().split('T')[0].split('-')
 		let datePrefix = ''
-		if (type === 'notes' || type === 'checkin') {
+		if (type === 'notes') {
 			datePrefix = `${year}/${month}/${day}/`
 		}
-		let filename = ''
-		if (type === 'checkin') {
-			filename = `${dir}/${type}/${datePrefix}${slug}.json`
-		} else {
-			filename = `${dir}/${type}/${datePrefix}${slug}.md`
-		}
+		const filename = `${dir}/${type}/${datePrefix}${slug}.md`
 
 		return {
 			'filename': filename,
@@ -93,7 +88,6 @@ const content = {
 		if (data['read-of']) return 'read'
 		if (data['listen-of']) return 'listen'
 		if (data['play-of']) return 'play'
-		if (data['checkin']) return 'checkin'
 		return 'notes'
 	},
 
